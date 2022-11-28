f9c56cb2eb79c3705de1d2e1cf8853d5b64eaae5
Tibovc <tibo.vancraenenbroeck@live.be>
2022-11-28 20:26:38 +0100

    Oefening 1

---
+++ Oefening01/Oefening01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefening01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Deze app vraagt willekeurige getallen aan de gebruiker.
+             * Stopt pas als de gebruiker op enter drukt.
+             */
+            List<int> getallen = new List<int>();
+            bool volgende = true;
+
+            while (volgende)
+            {
+                Console.WriteLine("Willekeurig getal: ");
+                string getal = Console.ReadLine();
+                if(getal != "")
+                {
+                    volgende = false;
+                }
+                else
+                {
+                    getallen.Add(getal);
+                }
+
+            }
+
+            Console.WriteLine("De gekozen getallen zijn:");
+            foreach (int g in getallen)
+            {
+                Console.WriteLine(g);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
